bf0cede85646c906a28a31fb5ea0e8ad978d070f
baraka0123 <barakaarnauld@gmail.com>
2026-01-31 23:38:30 -0800

    Update User.js

--- models/User.js
+++ models/User.js
@@ -1,12 +1,15 @@
 const mongoose = require("mongoose");
 
-const userSchema = new mongoose.Schema({
-  name: { type: String, required: true },
-  email: { type: String, required: true, unique: true },
-  password: { type: String, required: true },
-  role: { type: String, enum: ["patient", "clinician"], required: true },
-  licenseNumber: { type: String }, // only for clinician
-  specialty: { type: String },     // only for clinician
-}, { timestamps: true });
+const userSchema = new mongoose.Schema(
+  {
+    name: { type: String, required: true },
+    email: { type: String, required: true, unique: true },
+    password: { type: String, required: true },
+    role: { type: String, enum: ["patient", "clinician"], required: true },
+    licenseNumber: { type: String }, // only for clinician
+    specialty: { type: String },     // only for clinician
+  },
+  { timestamps: true }
+);
 
 module.exports = mongoose.model("User", userSchema);
